dd883b6f56ceae8e593786af5947f022336c255e
LAMurakami <GitHub@LAMurakami.com>
2022-06-28 20:29:00 -0800

    Add script to rebuild efs mlocate database

---
+++ local_scripts/locate-mnt-efs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+renice +19 -p $$ >/dev/null 2>&1
+ionice -c2 -n7 -p $$ >/dev/null 2>&1
+updatedb --require-visibility no \
+--output /mnt/efs/mlocate.db \
+--database-root /mnt/efs
+
+# Script to rebuild the /mnt/efs locate database for NFS client users
+
+# To run this once
+# sudo time -v /usr/local/scripts/locate-mnt-efs.sh
+
+# To have the system run this daily
+# sudo ln -s /usr/local/scripts/locate-mnt-efs.sh /etc/cron.daily/mlocate-mnt-efs
