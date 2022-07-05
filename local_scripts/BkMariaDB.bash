8154898f1ee2027514677100ffacab9d2ecb559b
LAMurakami <GitHub@LAMurakami.com>
2022-07-05 14:21:46 -0800

    Supress progress reporting by aws s3 cp upload

--- local_scripts/BkMariaDB.bash
+++ local_scripts/BkMariaDB.bash
@@ -5,6 +5,6 @@ BD=/mnt/ak20-Bk/Bk2/${BS}/Day/$(date +'%y%m%d')
 
 # Put the daily lam and wikidb backups in S3
 
-aws s3 cp ${BD}/Bk-20-MySQL.lam.sql.gz s3://lamurakami
-aws s3 cp ${BD}/Bk-20-MySQL.wikidb.sql.gz s3://lamurakami
+aws s3 cp ${BD}/Bk-20-MySQL.lam.sql.gz s3://lamurakami --no-progress
+aws s3 cp ${BD}/Bk-20-MySQL.wikidb.sql.gz s3://lamurakami --no-progress
 
