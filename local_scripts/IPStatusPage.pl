50e334210b78100f33935f577f6372a578436faa
LAMurakami <GitHub@LAMurakami.com>
2022-07-29 21:10:51 -0800

    ServerAdmin@LAMurakami.com

--- local_scripts/IPStatusPage.pl
+++ local_scripts/IPStatusPage.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-# $Id: IPStatusPage.pl,v 1.7 2015/05/09 07:38:20 lam Exp lam $
 
 my $cfDir = '/etc/LAM'; my $configFile = "$cfDir/IPStatus.conf";
 open (CONFIG, $configFile)                             # Open configuration file
@@ -97,7 +96,7 @@ my $page = <<END_OF_PAGE;
 <head><title>LAM Alaska Connection Status</title>
 <link rel="Shortcut Icon" href="favicon.ico" />
 <link rel="stylesheet" type="text/css" href="Public/Style.css" />
-<meta name="Author" content="www.ServerAdmin\@lam1.us" />
+<meta name="Author" content="www.ServerAdmin\@LAMurakami.com" />
 <!--
                               By: Lawrence A. Murakami - Fairbanks, Alaska -->
 
