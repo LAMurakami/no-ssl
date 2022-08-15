420fb2f0c198431d7f09ec5e36fc3573e88cca35
LAMurakami <GitHub@LAMurakami.com>
2022-08-14 18:33:45 -0800

    Use plocate for ak20-Bk and ak20-ext4 dbs

--- local_scripts/locate-mnt-ak20-Bk.sh
+++ local_scripts/locate-mnt-ak20-Bk.sh
@@ -2,15 +2,13 @@
 renice +19 -p $$ >/dev/null 2>&1
 ionice -c2 -n7 -p $$ >/dev/null 2>&1
 updatedb --require-visibility no \
---prunepaths /mnt/ak20-Bk/lost+found \
---output /mnt/ak20-Bk/mlocate.db \
+--prunepaths "/mnt/ak20-Bk/lost+found /mnt/ak20-Bk/timeshift" \
+--output /mnt/ak20-Bk/plocate.db \
 --database-root /mnt/ak20-Bk
+
+chmod a+r /mnt/ak20-Bk/plocate.db
 
 # Script to rebuild the /mnt/ak20-Bk locate database for NFS client users
 
 # To run this once
 # sudo time -v /var/www/no-ssl/local_scripts/locate-mnt-ak20-Bk.sh
-
-# To have the system run this daily
-# sudo ln -s /var/www/no-ssl/local_scripts/locate-mnt-ak20-Bk.sh \
-# /etc/cron.daily/mlocate-mnt-ak20-Bk
--- local_scripts/locate-mnt-ak20-ext4.sh
+++ local_scripts/locate-mnt-ak20-ext4.sh
@@ -3,14 +3,12 @@ renice +19 -p $$ >/dev/null 2>&1
 ionice -c2 -n7 -p $$ >/dev/null 2>&1
 updatedb --require-visibility no \
 --prunepaths /mnt/ak20-ext4/lost+found \
---output /mnt/ak20-ext4/mlocate.db \
+--output /mnt/ak20-ext4/plocate.db \
 --database-root /mnt/ak20-ext4
+
+chmod a+r /mnt/ak20-ext4/plocate.db
 
 # Script to rebuild the /mnt/ak20-ext4 locate database for NFS client users
 
 # To run this once
 # sudo time -v /var/www/no-ssl/local_scripts/locate-mnt-ak20-ext4.sh
-
-# To have the system run this daily
-# sudo ln -s /var/www/no-ssl/local_scripts/locate-mnt-ak20-ext4.sh \
-# /etc/cron.daily/mlocate-mnt-ak20-ext4
