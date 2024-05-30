5ffe7233ac6a2f502a4e6286d542491060fd34cf
LAMurakami <GitHub@LAMurakami.com>
2024-05-29 17:28:23 -0800

    Put the daily lam and wikidb backups on AWS EFS

--- local_scripts/BkMariaDB.bash
+++ local_scripts/BkMariaDB.bash
@@ -8,3 +8,7 @@ BD=/mnt/ak20-Bk/Bk2/${BS}/Day/$(date +'%y%m%d')
 aws s3 cp ${BD}/Bk-20-MySQL.lam.sql.gz s3://lamurakami --no-progress
 aws s3 cp ${BD}/Bk-20-MySQL.wikidb.sql.gz s3://lamurakami --no-progress
 
+# Put the daily lam and wikidb backups on AWS EFS
+
+scp -qp ${BD}/Bk-20-MySQL.lam.sql.gz aws:/mnt/efs
+scp -qp ${BD}/Bk-20-MySQL.wikidb.sql.gz aws:/mnt/efs
