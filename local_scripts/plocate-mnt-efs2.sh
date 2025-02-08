affd6676ec83477b79377fd94175789d3967e55c
LAMurakami <GitHub@LAMurakami.com>
2025-02-07 23:16:33 -0900

    Add plocate-mnt-efs2.sh daily cron

---
+++ local_scripts/plocate-mnt-efs2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+renice +19 -p $$ >/dev/null 2>&1
+ionice -c2 -n7 -p $$ >/dev/null 2>&1
+updatedb --require-visibility no \
+--output /mnt/efs2/plocate.db \
+--database-root /mnt/efs2
+
+# Script to rebuild the /mnt/efs2 plocate database for NFS client users
+
+# To run this once
+# sudo time -v /var/www/no-ssl/local_scripts/plocate-mnt-efs2.sh
+
+# To have the system run this daily
+# sudo ln -s /var/www/no-ssl/local_scripts/plocate-mnt-efs2.sh \
+# /etc/cron.daily/plocate-mnt-efs2.sh
--- plocate.sh
+++ plocate.sh
@@ -1,1 +1,1 @@
-export LOCATE_PATH=/mnt/efs/plocate.db
+export LOCATE_PATH=/mnt/efs/plocate.db:/mnt/efs2/plocate.db:/mnt/efs3/plocate.db
