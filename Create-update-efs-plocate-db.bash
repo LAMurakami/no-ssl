e340b8253688b6fc6cd39aa4ae9c5a46865b6a51
LAMurakami <GitHub@LAMurakami.com>
2025-02-09 17:27:25 -0900

    Add Create-update-efs-mlocate-db.bash
    Add local_scripts/mlocate-mnt-efs{,2,3}.sh
    
    Modify Create-update-efs-plocate-db.bash to add
    /etc/profile.d/mlocate.sh which sets LOCATE_PATH for all.

--- Create-update-efs-plocate-db.bash
+++ Create-update-efs-plocate-db.bash
@@ -7,7 +7,7 @@ for EFS in efs efs2 efs3 ; do
             echo "/mnt/${EFS}/plocate.db does not exist"
             echo "Create /mnt/${EFS}/plocate.db"
             /var/www/no-ssl/local_scripts/plocate-mnt-${EFS}.sh # Build
-            chmod o+r /mnt/${EFS}/plocate.db
+            chmod o+r /mnt/${EFS}/plocate.db # Allow all to read
         else
             one_day_ago=$(date -d 'now - 1 day' +%s)
             file_time=$(date -r "/mnt/${EFS}/plocate.db" +%s)
@@ -15,9 +15,16 @@ for EFS in efs efs2 efs3 ; do
                 echo "/mnt/${EFS}/plocate.db is older than 1 day"
                 echo "update /mnt/${EFS}/plocate.db"
                 /var/www/no-ssl/local_scripts/plocate-mnt-${EFS}.sh # Update
+                chmod o+r /mnt/${EFS}/plocate.db # Allow all to read
             fi
         fi
     else
         echo "/mnt/${EFS} directory does not exist."
     fi
 done
+
+SYSTEM_LOCATE_PATH='/mnt/efs/plocate.db:/mnt/efs2/plocate.db'
+if [ ! -f /mnt/efs3/plocate.db ] ; then
+    SYSTEM_LOCATE_PATH="${SYSTEM_LOCATE_PATH}:/mnt/efs3/plocate.db"
+fi
+echo "export LOCATE_PATH=${SYSTEM_LOCATE_PATH}" > /etc/profile.d/plocate.sh
